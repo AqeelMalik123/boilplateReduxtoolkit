8a08bf983e79f7d90d36939309f3eb75946ef884
unknown <maqeel191@gmail.com>
2024-07-13 04:29:02 -0700

    envFile

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import UserList from './pages/user/userList';
+import CreateForm from './component/Form/createForm';
+// import Products from './component/Products';
+
+// import Nabar from './component/Navbar';
+// import BasicTable from './table/table';
+// import Basic from './form';
 
 function App() {
   return (
-    <>
-    <UserList/>
-    </>
+    <div className="App">
+      {/* <Nabar/> */}
+      {/* <BasicTable/> */}
+      {/* <Basic/> */}
+      
+    {/* <MyForm/> */}
+    {/* <Products/> */}
+    <CreateForm/>
+    </div>
   );
 }
 
--- src/component/Form/createForm.tsx
+++ src/component/Form/createForm.tsx
@@ -1,6 +1,53 @@
+import { Box, Grid, TextField, Typography } from "@mui/material"
+
 const CreateForm=()=>{
 
     return(
-      <></>
+      <>
+      
+       <Grid container sx={{backgroundColor:"pink"}}>
+      
+
+            <Grid item xs={12} md={6}>
+                <Box>
+
+                   <Typography sx={{ fontfamily:' Questrial',
+fontSize: "32px",
+fontWeight: "400",
+lineHeight: "39.84px",
+textAlign: "left"}} >
+                    Join Our Job Portal
+                   </Typography>
+                   <Typography sx={{ fontfamily:' Questrial',
+fontSize: "14px",
+fontWeight: "400",
+lineHeight: "21.35px",
+}} >
+
+                     job Seeker apply, Employers post ,search,manage
+                   </Typography>
+                   <Box>
+                   <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                   </Box>
+                </Box>
+
+           
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Box>
+
+                  <img src="/login.png"/>
+                  <Typography>
+                  Here, you can find the right fit for your career or hire talent that fits your company's requirements.
+                  </Typography>
+
+              </Box>
+
+            </Grid>
+       
+
+       </Grid>
+      </>
     )
 }
+export default CreateForm
