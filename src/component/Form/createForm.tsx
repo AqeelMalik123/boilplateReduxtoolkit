79034375678e64528920f8c584af9e58dcbde5ab
unknown <maqeel191@gmail.com>
2024-07-13 07:26:27 -0700

    merge

--- src/component/Form/createForm.tsx
+++ src/component/Form/createForm.tsx
@@ -1,5 +1,9 @@
-import { Box, Grid, TextField, Typography } from "@mui/material"
+import { Box, Checkbox, FormControlLabel, Grid, TextField, Typography } from "@mui/material"
+// import { MdOutlineMail } from "react-icons/md";
 
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import KeyOutlinedIcon from '@mui/icons-material/KeyOutlined';
+import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined';
 const CreateForm=()=>{
 
     return(
@@ -8,26 +12,57 @@ const CreateForm=()=>{
        <Grid container sx={{backgroundColor:"pink"}}>
       
 
-            <Grid item xs={12} md={6}>
-                <Box>
-
+            <Grid item xs={12} md={6} sx={{backgroundColor:"white"}}>
+                <Box sx={{}}>
+                    <Box>
                    <Typography sx={{ fontfamily:' Questrial',
 fontSize: "32px",
 fontWeight: "400",
-lineHeight: "39.84px",
-textAlign: "left"}} >
+lineHeight: "39.84px",color:"#034158"
+// textAlign: "left"
+
+}} >
                     Join Our Job Portal
                    </Typography>
                    <Typography sx={{ fontfamily:' Questrial',
 fontSize: "14px",
 fontWeight: "400",
-lineHeight: "21.35px",
+lineHeight: "21.35px",color:"#034158"
 }} >
 
                      job Seeker apply, Employers post ,search,manage
                    </Typography>
+                   </Box>
                    <Box>
-                   <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                    <Box sx={{my:1}}>
+                   <TextField
+            size="small"
+            // fullWidth
+            placeholder="Enter your email"
+            // value={search}
+            // onChange={(e) => {
+            //   setsearch(e.target.value);
+            // }}
+            InputProps={{
+              startAdornment: <EmailOutlinedIcon style={{ color: 'lightgray' }} />
+            }}
+          /></Box>
+          <Box sx={{my:1}}>
+                   <TextField
+            size="small"
+            // fullWidth
+            placeholder="Enter your Password"
+            // value={search}
+            // onChange={(e) => {
+            //   setsearch(e.target.value);
+            // }}
+            InputProps={{
+              startAdornment: <KeyOutlinedIcon style={{ color: 'lightgray' }} />,
+              endAdornment: <VisibilityOffOutlinedIcon style={{ color: 'lightgray' }} />
+            }}
+          /></Box>
+
+<FormControlLabel control={<Checkbox defaultChecked />} label="Accept term and Condition" />
                    </Box>
                 </Box>
 
